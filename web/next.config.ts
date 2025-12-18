b59a529ae923348e987f4e3498b585e30ad712b9
tudz <tranhoangtu150106@gmail.com>
2025-12-18 09:36:26 +0700

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    // Bỏ qua lỗi ESLint để có thể build thành công
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Bỏ qua lỗi TypeScript để không bị dừng quá trình build
     ignoreBuildErrors: true,
   },
+  // Thêm dòng này để xử lý lỗi render trang Checkout
+  output: 'standalone', 
 };
 
 export default nextConfig;
